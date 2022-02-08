d89b1328af343bf2b4d7113fd1f09cc7aaab281f
Artem Treiko <a.treiko92@gmail.com>
2022-02-08 18:31:35 +0200

    task

--- javascript/study/functional-programming/compose/scripts.js
+++ javascript/study/functional-programming/compose/scripts.js
@@ -3,31 +3,31 @@
  * Композируем
  */
 
- const upperCase = str => str.toUpperCase();
+const upperCase = str => str.toUpperCase();
 
- const exclaim = str => `${str}!`;
- 
- const repeat = str => `${str} `.repeat(3)
- 
- console.log(
-   repeat(exclaim(upperCase('Fire')))
- );
- 
- console.log(
-   exclaim(repeat(upperCase('Fire')))
- );
+const exclaim = str => `${str}!`;
+
+const repeat = str => `${str} `.repeat(3)
+
+console.log(
+  repeat(exclaim(upperCase('Fire')))
+);
+
+console.log(
+  exclaim(repeat(upperCase('Fire')))
+);
  
  //! ================================================
  
- const compose = (...functions) => {
-   return (initialValue) => 
-     functions.reduceRight((result, func) => func(result), initialValue)
- }
+const compose = (...functions) => {
+  return (initialValue) => 
+    functions.reduceRight((result, func) => func(result), initialValue)
+}
  
  const withCompose = compose(
-   repeat,
-   exclaim,
-   upperCase
- );
+  repeat,
+  exclaim,
+  upperCase
+);
  
  console.log(withCompose('god'));
